a6f5a8a75b0f90a344e4cf39d13050044046af72
imada-dchain <takeru.imada@gmail.com>
2024-06-08 06:11:33 +0900

    add: complete file uploading function and make proposal function

--- wagmi-project/src/app/propose/page.tsx
+++ wagmi-project/src/app/propose/page.tsx
@@ -7,7 +7,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 
 import { useWriteContract, type BaseError } from "wagmi";
-import { Address, toHex } from "viem";
+import { Address, Hex } from "viem";
 
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
@@ -17,6 +17,7 @@ import FormInput from "@/components/form-input";
 import { abi } from "@/lib/abi/TextDAOFacade";
 import { proposalSchema, type proposalSchemaType } from "@/lib/schema/schema";
 import { createNewProposal, queryClient } from "@/lib/http";
+import { getDigest } from "@/lib/helper";
 import { account } from "@/lib/account";
 
 const INPUTS = [
@@ -74,6 +75,7 @@ export default function ProposePage() {
     },
     onSuccess: (data) => {
       localStorage.setItem("IPFS_CID", data);
+      localStorage.setItem("IPFS_DIGEST", getDigest(data));
       toast({
         title: "Success to upload to IPFS",
         description: "Proposal data is uploaded.",
@@ -87,6 +89,8 @@ export default function ProposePage() {
         description: error.message,
       });
     },
+
+    // Note: Invalidate the cache when the mutation is successful
   });
 
   const form = useForm<proposalSchemaType>({
@@ -99,7 +103,7 @@ export default function ProposePage() {
       header: {
         id: BigInt(id),
         currentScore: BigInt(0),
-        metadataURI: toHex(ipfs_cid, { size: 32 }),
+        metadataURI: ipfs_cid as Hex,
         tagIds: [],
       },
       cmd: {
@@ -130,7 +134,6 @@ export default function ProposePage() {
       {
         onSuccess: (data) => {
           setIsHandling(false);
-          localStorage.removeItem("IPFS_CID");
           toast({
             title: "Transaction confirmed",
             description: `Transaction Hash: ${data}`,
@@ -155,14 +158,14 @@ export default function ProposePage() {
 
   async function handleSubmit(data: proposalSchemaType) {
     if (isError) {
-      console.log("isError");
       setIsHandling(true);
-      const ipfs_cid = localStorage.getItem("IPFS_CID");
+      const ipfs_cid = localStorage.getItem("IPFS_DIGEST");
       sendTransaction(data.id, ipfs_cid!);
       return;
     }
-    const ipfs_cid = await mutateAsync(data);
-    sendTransaction(data.id, ipfs_cid);
+    await mutateAsync(data);
+    const ipfs_cid = localStorage.getItem("IPFS_DIGEST");
+    sendTransaction(data.id, ipfs_cid!);
   }
 
   return (
