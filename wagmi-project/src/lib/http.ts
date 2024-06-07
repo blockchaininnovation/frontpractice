f3f3d756ba3162b20bfc23edcf79fbbe49f14236
imada-dchain <takeru.imada@gmail.com>
2024-06-08 06:12:32 +0900

    refactor: create helper

--- wagmi-project/src/lib/http.ts
+++ wagmi-project/src/lib/http.ts
@@ -1,26 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
+import { getPinataFormat } from "./helper";
 import { type proposalSchemaType } from "./schema/schema";
 
 export const queryClient = new QueryClient();
 
-function format(data: proposalSchemaType) {
-  return {
-    pinataOptions: { cidVersion: 1 },
-    pinataMetadata: { name: data.id.toString() },
-    pinataContent: { title: data.title, description: data.description },
-  };
-}
-// function format(data: proposalSchemaType) {
-//   return {
-//     pinataOptions: { cidVersion: 1 },
-//     pinataMetadata: { name: data.id },
-//     pinataContent: { title: data.title, description: data.description },
-//   };
-// }
-
 export async function createNewProposal(proposalData: proposalSchemaType) {
-  const data = format(proposalData);
+  const data = getPinataFormat(proposalData);
   const response = await fetch(
     "https://api.pinata.cloud/pinning/pinJSONToIPFS",
     {
