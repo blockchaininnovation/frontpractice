730faf584487dcd8644d6c9f6390a5a7e5f9f541
imada-dchain <takeru.imada@gmail.com>
2024-06-12 09:00:43 +0900

    add: vote page

--- wagmi-project/src/app/vote/page.tsx
+++ wagmi-project/src/app/vote/page.tsx
@@ -1,3 +1,63 @@
+"use client";
+
+import { useWriteContract, type BaseError } from "wagmi";
+import { type Address } from "viem";
+
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+
+import { abi } from "@/lib/abi/TextDAOFacade";
+import { account } from "@/lib/account";
+
 export default function VotePage() {
-  return <div>Vote</div>;
+  const { toast } = useToast();
+  const { isPending, writeContract } = useWriteContract({
+    mutation: {
+      retry: 3,
+      onMutate: () =>
+        toast({
+          title: "Sending transaction...",
+          description: "This may take a while. Please wait...",
+        }),
+    },
+  });
+
+  function handleSubmit() {
+    const args = {
+      proposalId: BigInt(0),
+      headerIds: [BigInt(0), BigInt(1), BigInt(2)] as const,
+    };
+
+    writeContract(
+      {
+        address: process.env.NEXT_PUBLIC_CONTRACT_ADDR! as Address,
+        abi,
+        functionName: "voteHeaders",
+        args: [args.proposalId, args.headerIds],
+        account,
+      },
+      {
+        onSuccess: (data) => {
+          toast({
+            title: "Transaction confirmed",
+            description: `Transaction Hash: ${data}`,
+          });
+        },
+        onError: (error) => {
+          toast({
+            variant: "destructive",
+            title: error.name,
+            description: (error as BaseError).shortMessage,
+          });
+        },
+      }
+    );
+  }
+
+  return (
+    <div className="px-20 py-5">
+      <h1 className="text-xl font-bold py-10">Vote Page</h1>
+      <div></div>
+    </div>
+  );
 }
