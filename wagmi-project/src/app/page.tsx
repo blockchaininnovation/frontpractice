5bccf6aff943cbaa323936ed6711a4d8b747149a
imada-dchain <takeru.imada@gmail.com>
2024-06-14 20:06:36 +0900

    add: some instruction

--- wagmi-project/src/app/page.tsx
+++ wagmi-project/src/app/page.tsx
@@ -1,8 +1,23 @@
 function App() {
   return (
-    <>
-      <div>Top</div>
-    </>
+    <div className="px-20 py-5">
+      <h1 className="text-xl font-bold py-10">Top</h1>
+      <article>
+        <h3 className="text-lg">使い方</h3>
+        <br />
+        <ol className="list-decimal ml-5">
+          <li>
+            右上の MetaMask ボタンを押してウォレットをコネクトする（チェーンを
+            Sepolia にしてください）
+          </li>
+          <li>Initialize から初期設定を済ませる（講師が済ませています）</li>
+          <li>Propose から提案を作成する（講師が行います）</li>
+          <li>Vote から提案に投票を行う</li>
+          <li>Tally で投票を集計する（講師が行います）</li>
+          <li>Contract Call から投票結果を確認する</li>
+        </ol>
+      </article>
+    </div>
   );
 }
 
