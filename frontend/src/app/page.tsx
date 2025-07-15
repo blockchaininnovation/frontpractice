a837c620cc02fa411dcb147194aab02ffc39fc6c
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2025-07-15 13:49:20 +0900

    使い方を修正。

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,15 +7,17 @@ function App() {
         <br />
         <ol className="list-decimal ml-5">
           <li>
-            右上の MetaMask ボタンを押してウォレットをコネクトする（チェーンを
-            Sepolia にしてください）
+            右上の MetaMask ボタンを押してウォレットをコネクトする（チェーンをanvilにしてください）
           </li>
-          <li>Initialize から初期設定を済ませる（講師が済ませています）</li>
-          <li>Propose から提案を作成する（講師が行います）</li>
+          <li>Initialize から初期設定を済ませる</li>
+          <li>Propose から提案を作成する</li>
           <li>Proposalから作成したProposeが取得できるか確認する</li>
           <li>MemberJoin から自分のアカウントを登録する</li>
-          <li>Membersから登録した自分のアカウントが取得できるか確認する（実装演習）</li>
-          <li>Vote から提案に投票を行う（実装演習）</li>
+          <li>Membersから登録した自分のアカウントが取得できるか確認する</li>
+          <li>Vote から提案に投票を行う</li>
+          <li>Proposalで投票ができているか確認する</li>
+          <li>Onboard Imageで自身のアイコンを登録する</li>
+          <li>Membersから、登録したアイコンのIPFS、署名情報を確認する。</li>
         </ol>
       </article>
     </div>
